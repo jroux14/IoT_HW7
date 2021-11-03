b9534a6b58fbc1e6709f3ac54285aed6477e5011
jroux14 <jroux14@gmail.com>
2021-11-03 22:40:50 +0000

    Initial Commit

---
+++ myApp/derek_LED.h
@@ -0,0 +1,17 @@
+#define LED_PATH "/sys/class/leds/beaglebone:green:usr"
+
+class LED{
+   	private:
+      		std::string path;
+      		int number;
+      		virtual void writeLED(std::string filename, std::string value);
+      		virtual void removeTrigger();
+   	public:
+      		LED(int number);
+      		virtual void turnOn();
+      		virtual void turnOff();
+      		virtual void flash(std::string delayms);
+      		virtual void outputState();
+		virtual void blink(int ledNum, int num);
+     		virtual ~LED();
+};
